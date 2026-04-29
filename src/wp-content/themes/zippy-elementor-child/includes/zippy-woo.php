0630839b187088486d355ff708dae595d73c6682
Lucass1611 <chuong.nguyenquoc@floatingcube.com>
2026-04-29 11:12:19 +0700

    Update Sprintf

--- src/wp-content/themes/zippy-elementor-child/includes/zippy-woo.php
+++ src/wp-content/themes/zippy-elementor-child/includes/zippy-woo.php
@@ -249,3 +249,18 @@ function zippy_render_shipping_method_notes($method, $index)
     </div>
 <?php
 }
+
+function enqueue_sprintf_for_elementor()
+{
+    
+    if (did_action('elementor/loaded')) {
+        wp_enqueue_script(
+            'sprintf-js',
+            'https://cdnjs.cloudflare.com/ajax/libs/sprintf/1.1.2/sprintf.min.js',
+            [],
+            '1.1.2',
+            true
+        );
+    }
+}
+add_action('elementor/editor/after_enqueue_scripts', 'enqueue_sprintf_for_elementor');

0cf75595ce3bd5f330ec51a0713af3f88a09db4b
Lucass1611 <chuong.nguyenquoc@floatingcube.com>
2026-04-29 15:40:59 +0700

    Update code

--- src/wp-content/themes/zippy-elementor-child/includes/zippy-woo.php
+++ src/wp-content/themes/zippy-elementor-child/includes/zippy-woo.php
@@ -6,6 +6,39 @@ function remove_my_account_downloads($items)
     return $items;
 }
 
+add_filter('woocommerce_login_redirect', 'zippy_redirect_customer_login_to_shop', 10, 2);
+add_filter('login_redirect', 'zippy_redirect_customer_wp_login_to_shop', 10, 3);
+
+function zippy_get_shop_redirect_url()
+{
+    $shop_url = function_exists('wc_get_page_permalink') ? wc_get_page_permalink('shop') : '';
+
+    return $shop_url ? $shop_url : home_url('/shop/');
+}
+
+function zippy_user_is_admin_role($user)
+{
+    return $user instanceof WP_User && user_can($user, 'manage_options');
+}
+
+function zippy_redirect_customer_login_to_shop($redirect, $user)
+{
+    if (zippy_user_is_admin_role($user)) {
+        return $redirect;
+    }
+
+    return zippy_get_shop_redirect_url();
+}
+
+function zippy_redirect_customer_wp_login_to_shop($redirect_to, $requested_redirect_to, $user)
+{
+    if (is_wp_error($user) || zippy_user_is_admin_role($user)) {
+        return $redirect_to;
+    }
+
+    return zippy_get_shop_redirect_url();
+}
+
 
 add_action('woocommerce_after_shop_loop_item', 'shop_loop_add_to_cart', 15);
 
